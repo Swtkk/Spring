e0497414a11f073d78d1f84f4d203f8640c8173a
Marcin <marcin123523@wp.pl>
2023-09-20 19:59:10 +0200

    Dodano wyjatek istniejacego juz streamera o podanym id

---
+++ swietlik/src/main/java/com/swietlik/swietlik/controller/AlreadyExistException.java
@@ -0,0 +1,11 @@
+package com.swietlik.swietlik.controller;
+
+import org.springframework.core.NestedCheckedException;
+
+public class AlreadyExistException extends NestedCheckedException {
+    String message;
+
+    public AlreadyExistException(String message) {
+        super(message);
+    }
+}
